dfb2e17d5839bcbb5bd229633def93c048baa7e7
Aniket <aniketmotwani52@gmail.com>
2023-01-09 00:32:39 +0530

    commiting_login_form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Login from "./components/login";
+import Register from "./components/register";
+import Form from "./components/form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+var isRegistered = false;
+//true-login page
+//false- register page
+
+const present_time = new Date().toLocaleTimeString();
+
+
+function App(){
+
+  const [time, setTime]=useState(present_time);
+  // console.log(time);
+
+  function change()
+  {
+      const newTime = new Date().toLocaleTimeString();
+      setTime(newTime);  
+  }
+  
+  setInterval(change,1000);
+  return(
+    <div className="container">
+
+    {/* { isRegistered && <Login></Login>}
+    {  !(isRegistered) && <Register></Register>} */}
+
+    <Form isRegister={isRegistered}></Form>
+    
+    <div>
+        <h1> {time} </h1>
+        <br></br>
+        <button  className="time" onClick={change} > Get Time</button>
+
     </div>
-  );
+
+    </div>
+  )
 }
 
 export default App;
+
